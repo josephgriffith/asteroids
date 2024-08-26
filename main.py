668e71a04af50dde69c19c57e9b6930ca660b6c0
josephgriffith <joseph.griffith17@alumni.colostate.edu>
2024-08-26 12:53:32 -0600

    added frame limiting

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     frames = 0
     pygame.font.init()
     font = pygame.font.SysFont('Consolas', 32)
@@ -25,6 +28,8 @@ def main():
         txt = font.render(str(frames), False, (32, 32, 32))
         screen.blit(txt, (20,20))
         pygame.display.flip()
+
+        dt = clock.tick(30)/1000
 
 
 
